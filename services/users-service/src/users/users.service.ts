b9a6f472d376abd14cb4a0ff2bdbb23989907357
AlejandroTejedaM <alex2227@hotmail.com>
2026-03-16 01:35:56 -0600

    feat(users-service): add UsersService with findByEmail and findById methods

--- services/users-service/src/users/users.service.ts
+++ services/users-service/src/users/users.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
+  ) {}
+
+  public async getUserByEmail(email: string): Promise<User | null> {
+    return this.userRepository.findOne({ where: { email } });
+  }
+
+  public async getUserById(id: string): Promise<User | null> {
+    return this.userRepository.findOne({ where: { id } });
+  }
+}
